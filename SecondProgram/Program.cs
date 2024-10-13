2258071a30c909abec03c03381fea06eb07d0646
drutuja-patil <drutujapatil@gmail.com>
2024-10-13 13:52:14 +0530

    Add new console statement

--- SecondProgram/Program.cs
+++ SecondProgram/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello,It's My Second Repo");
         }
     }
 }
